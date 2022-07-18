851cd6f1e475277e01dab37b338a372668662b50
AbdulSamad <47020656+4samad@users.noreply.github.com>
2022-07-18 17:52:29 +0400

    update api

--- backend/routes/nft.js
+++ backend/routes/nft.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 /* GET NFT Data. */
-router.get('/:id', (req, res) => {
-  res.send('NFT ' + req.params.id + ' here soon!')
+router.get('/:id', (req, res, next) => {
+  fs.readFile('assets/heisters-data.json', (err, data) => {
+    if (err) throw err;
+    const heisters = JSON.parse(data);
+    const heister = heisters[req.params.id];
+    res.json(heister);
+  });
 })
 
 module.exports = router;
